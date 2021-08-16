a40330437071c499c0884a007a0a3e3b72d77ab5
Lin Zhang <linzhang294@gmail.com>
2021-08-16 00:37:05 -0400

    adds sound functionality

--- NoteAppCoreData/Controllers/Wave Animation/FT/ZeroFTVC.swift
+++ NoteAppCoreData/Controllers/Wave Animation/FT/ZeroFTVC.swift
@@ -9,14 +9,49 @@
     
 import UIKit
 import Lottie
+import AVFoundation
+
 
 class ZeroFTVC: UIViewController {
+    
+    @IBOutlet var button: UIButton!
+    
+    var player: AVAudioPlayer?
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Added navigation button
         self.navigationItem.leftBarButtonItem = UIBarButtonItem(title: "Done", style: .plain, target: self, action: #selector(backToInitial(sender:)))
         lottieAnimation()
+    }
+    
+    @IBAction func didTapButton() {
+        if let player = player, player.isPlaying{
+            // stop playback
+            
+            player.stop()
+        }
+        else{
+            // set up player, and play
+            let urlString = Bundle.main.path(forResource: "seagull-beach-sound-effect", ofType: "mp3")
+            
+            do{
+                try AVAudioSession.sharedInstance().setMode(.default)
+                try AVAudioSession.sharedInstance().setActive(true, options: .notifyOthersOnDeactivation)
+                
+                guard let urlString = urlString else{
+                    return
+                }
+                player = try AVAudioPlayer(contentsOf: URL(fileURLWithPath: urlString))
+                guard let player = player else {
+                    return
+                }
+                player.play()
+            }
+            catch{
+                print("something went wrong")
+            }
+        }
     }
     
     @objc func backToInitial(sender: AnyObject) {
